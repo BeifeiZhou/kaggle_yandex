a561ffd1616122291d830583e6f6a75d785aecea
seplanna <seplanna@gmail.com>
2015-09-06 14:09:09 +0300

    add start to serialize to struct

--- scripts/working with data.py
+++ scripts/working with data.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import struct
 
 class Working_with_data(object):
 
@@ -46,31 +47,17 @@ class Working_with_data(object):
         #assume that first M,then Q,then C
         #self.data_by_sessions = {}
         one_session = {}
-        self.people = {}
         with open(data_file) as data,open('../my_data/train', 'w') as result:
             for i,line in enumerate(data):
-                #if (i > 10**6):
-                #    break
                 if (i%10**6 == 0):
                     print(i)
                 line = line.strip().split('\t')
                 session_id = line[0]
-
-                #if session_id not in self.data_by_sessions and len(self.data_by_sessions.keys()) > 0 and len(line) != 4:
-                #    continue
                 if (len(line) == 4):
                     if (len(one_session.keys()) > 0):
                         result.write(json.dumps(one_session) + '\n')
                     one_session = {}
                     res = self.Get_data_with_type_of_record_M(line)
-
-                    #if (len(self.people.keys()) > 10 and res['userId'] not in self.people):
-                    #    continue
-                    #if (res['userId'] not in self.people):
-                    #    self.people[res['userId']] = [session_id]
-                    #else:
-                    #    self.people[res['userId']].append(session_id)
-
                     one_session = res
 
 
@@ -85,10 +72,42 @@ class Working_with_data(object):
     def __init__(self,data_file):
         self.Get_data(data_file)
 
+class Data_utils(object):
+
+    #def Get_url_in_struct(self,url_id, url_info):
+    #    #url_id,domain": "1084315", "time_passed": {}, "rank
+    #    if (not isinstance(url['time_passed'], int)):
+    #        url['tyme_passed'] = -1
+    #    a = struct.pack('iiii', int(url_id),int(url_info['domain']),int(url_info['time_passed']),int(url['rank']))
+    #    return a
+
+    #def Get_serp_in_struct(self, serp):
+
+
+    #def Get_data_to_bynary(self, session_file):
+    #    #format serp_id:{},...,serp_id:{},user_id:<>,day:<>
+    #    with open(session_file) as sessions, open(user_sessions_file, 'w') as user_session:
+    #        for line in sessions:
+    #            line = json.loads(line.strip())
+    #            n_serps = len(line.keys()) - 2
+
+
+    def Get_session_by_one_predicate(self, session_file, user_sessions_file, predicate):
+        res = {}
+        with open(session_file) as sessions, open(user_sessions_file, 'w') as user_session:
+            for line_n,line in enumerate(sessions):
+                if (line_n % 10**4 == 0):
+                    print(line_n)
+                line = json.loads(line.strip())
+                if (line[predicate] not in res):
+                    res[line[predicate]] = []
+                res[line[predicate]].append(line_n)
+            for key in res.keys():
+                user_session.write(key + '\t' + ','.join(str(i) for i in res[key]) + '\n')
+
+
 def main():
-    data = Working_with_data(sys.argv[1])
-    #for i,people in data.people:
-    #    with open('../my_data/user_' + str(i), 'w') as user:
-    #        for session in people:
-    #            user.write(data.data_by_sessions[session] + '\n\n\n')
+    #data = Working_with_data(sys.argv[1])
+    people = Data_utils()
+    people.Get_session_by_one_predicate(sys.argv[1], sys.argv[2], 'userId')
 main()
